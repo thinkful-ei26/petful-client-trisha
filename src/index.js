019ffe4e179395e5b3ceb648ad550335172310cc
kronicle114 <trisha.aguinaldo@gmail.com>
2019-01-25 10:20:17 -0500

    initialState working

--- src/index.js
+++ src/index.js
@@ -3,32 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Dashboard from './components/Dashboard';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 
-const catToAdopt = {
-  imageURL: 'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-  name: 'Fluffy',
-  sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street',
-};
-
-const dogToAdopt = {
-  imageURL:
-    'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-  name: 'Zeus',
-  sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away',
-};
+const state = store.getState();
+console.log('state from reducers',state);
 
 ReactDOM.render(
 <Dashboard 
-  catToAdopt={catToAdopt} 
-  dogToAdopt={dogToAdopt}
+  catToAdopt={state.catsReducer.cat} 
+  dogToAdopt={state.dogsReducer.dog}
 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
